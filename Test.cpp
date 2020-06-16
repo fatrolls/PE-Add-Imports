033d3323cdc14e2ff72e15eae255b33130a3b8fe
fatrolls <fatrolls@users.noreply.github.com>
2020-06-15 23:51:01 -0400

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,16 @@
+#include "PEFile.h"
+
+int main(int argc, char* argv[]) {
+	// Open the input file
+	PEFile pe("1.exe");
+
+	// Add "MessageBoxA" & "ShowWindow" functions to the import table
+	char* functions[] = { "MessageBoxA", "ShowWindow" };
+	pe.addImport("user32.dll", functions, 2);
+	
+	// Add a new section named ".at4re" with size "0x1000" byte
+	pe.addSection(".at4re", 0x1000, false);
+	
+	// Save the modified file
+	pe.saveToFile("1+.exe");
+}
